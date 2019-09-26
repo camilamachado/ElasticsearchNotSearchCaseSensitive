2a1d20dbbf0642c95e5e6f8efb80f63548da648f
Camila Melo Machado <dev7msdn@nddprint.com.br>
2019-09-26 16:54:46 -0300

    Criando endpoint prar criar um estudante

---
+++ Elasticsearch.NotSearchCaseSensitive.API/Features/Students/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Elasticsearch.NotSearchCaseSensitive.Application.Features.Students;
+using Elasticsearch.NotSearchCaseSensitive.API.Base;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Elasticsearch.NotSearchCaseSensitive.API.Features.Students
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentController(IMediator mediator) : base()
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Adiciona um estudante
+        /// </summary>
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> PostStudentAsync([FromBody] StudentCreate.Command command)
+        {
+            var result = await _mediator.Send(command);
+
+            return HandleCommand(result);
+        }
+    }
+}
---
+++ Elasticsearch.NotSearchCaseSensitive.Application/Features/Students/StudentCreate.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using System;
+using FluentValidation;
+using FluentValidation.Results;
+using System.Threading.Tasks;
+using System.Threading;
+using AutoMapper;
+using Elasticsearch.NotSearchCaseSensitive.Domain.Features.Students;
+using Elasticsearch.NotSearchCaseSensitive.Infra.Structs;
+using Unit = Elasticsearch.NotSearchCaseSensitive.Infra.Structs.Unit;
+
+namespace Elasticsearch.NotSearchCaseSensitive.Application.Features.Students
+{
+    public class StudentCreate
+    {
+        public class Command : IRequest<Result<Exception, Unit>>
+        {
+            public Guid MatriculationNumber { get; set; }
+            public string Name { get; set; }
+
+            public ValidationResult Validate()
+            {
+                return new Validator().Validate(this);
+            }
+
+            private class Validator : AbstractValidator<Command>
+            {
+                public Validator()
+                {
+                    RuleFor(st => st.MatriculationNumber).NotEmpty();
+                    RuleForEach(st => st.Name).NotEmpty();
+                }
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Exception, Unit>>
+        {
+            private readonly IStudentRepository _studentRepository;
+
+            public Handler(IStudentRepository studentRepository)
+            {
+                _studentRepository = studentRepository;
+            }
+
+            public async Task<Result<Exception, Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var student = Mapper.Map<Command, Student>(request);
+
+                var studentCallback = await _studentRepository.AddAsync(student);
+
+                if (studentCallback.IsFailure)
+                    return studentCallback.Failure;
+
+                return Unit.Successful;
+            }
+        }
+    }
+}
